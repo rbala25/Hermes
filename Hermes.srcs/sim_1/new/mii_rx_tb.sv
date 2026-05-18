1219dc1d2763d2f6cdd555669668248eed25b2a0
rbala25 <rishibala2007@gmail.com>
2026-05-18 13:19:40 -0400

    switching to mii because phy chip doesnt support rgmii

---
+++ Hermes.srcs/sim_1/new/mii_rx_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Rishi Ba;a
+// 
+// Create Date: 05/18/2026 01:18:45 PM
+// Design Name: 
+// Module Name: mii_rx_tb
+// Project Name: 
+// Target Devices: Arty A7 35t
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mii_rx_tb(
+
+    );
+endmodule
---
+++ Hermes.srcs/sources_1/new/mii_rx.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Rishi Bala
+// 
+// Create Date: 05/18/2026 01:18:13 PM
+// Design Name: 
+// Module Name: mii_rx
+// Project Name: 
+// Target Devices: Arty A7 35t
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mii_rx(
+
+    );
+endmodule
