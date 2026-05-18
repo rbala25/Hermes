b94c7ec970bd81262c826bd629c2e70c3e590a6a
rbala25 <rishibala2007@gmail.com>
2026-05-18 16:11:47 -0400

    fsm and rst setup

--- Hermes.srcs/sources_1/new/eth_parser.sv
+++ Hermes.srcs/sources_1/new/eth_parser.sv
@@ -24,12 +24,47 @@ module eth_parser(output logic [47:0] dest_mac, output logic [47:0] src_mac, out
                     output logic [7:0] payload_data, output logic payload_valid, output logic frame_done, output logic error,
                     input logic clk, input logic rst, input logic [7:0] data, input logic valid, input logic frame_active);
                     
-    typedef enum logic [2:0] {
+    typedef enum logic [2:0] { //fsm
         idle,
         destmac,
         srcmac,
         ether,
         payload
-    } state_t
-                    
+    } state_t;
+    
+    state_t state;
+    logic[3:0] cnt;
+    
+    logic frame_active_prev; //previous value
+    
+    logic [7:0] fcs_buf [0:3]; //shift register for FCS
+    logic [2:0] fcs_count;
+    
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            state <= idle;
+            cnt <= 0;
+            src_mac <= 0;
+            dest_mac <= 0;
+            ether_type <= 0;
+            header_valid <= 0;
+            payload_data <= 0;
+            payload_valid <= 0;
+            frame_done <= 0;
+            error <= 0;
+            fcs_count <= 0;
+            fcs_buf[0] <= 0;
+            fcs_buf[1] <= 0;
+            fcs_buf[2] <= 0;
+            fcs_buf[3] <= 0;
+            frame_active_prev <= 0;
+        end else begin
+            header_valid <= 0;
+            payload_valid <= 0;
+            frame_done <= 0;
+            error <= 0;
+            frame_active_prev <= frame_active;
+        
+        end
+    end          
 endmodule
