88b7ce85cb74f010af38290f2962b8fb6295ab24
rbala25 <rishibala2007@gmail.com>
2026-05-21 01:46:30 -0400

    mii fsm

--- Hermes.srcs/sources_1/new/mii_tx.sv
+++ Hermes.srcs/sources_1/new/mii_tx.sv
@@ -30,4 +30,53 @@ module mii_tx(
     output logic [3:0] txd,
     output logic tx_en
     );
+    
+typedef enum logic [1:0] {
+    idle, preamble, transmit
+} state_t;
+ 
+state_t state;
+logic [3:0] cnt;
+logic nibble_sel;
+logic [7:0] byte_buf;
+
+always_ff @(posedge tx_clk) begin
+    if(rst) begin
+        state <= idle;
+        cnt <= 0;
+        nibble_sel <= 0;
+        byte_buf <= 0;
+        txd <= 0;
+        tx_en <= 0;
+        ready <= 0;
+    end else begin
+        tx_en <= 0;
+        ready <= 0;
+        txd <= 4'h0;
+        
+        unique case (state)
+            idle: begin
+                nibble_sel <= 0;
+                cnt <= 0;
+                if(valid) begin
+                    state <= preamble;
+                end
+            end
+            
+            preamble: begin
+                tx_en <= 1;
+                cnt <= cnt + 1;
+                if(cnt > 15)begin
+                    txd <= 4'hd;
+                    state <= transmit;
+                    cnt <= 0;
+                end else txd <= 4'h5;
+            end
+            
+            transmit: begin
+            
+            end
+        endcase
+    end
+end
 endmodule
