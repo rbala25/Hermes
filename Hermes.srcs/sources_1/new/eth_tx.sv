5299f97167a7a5cef5738ce8c8db73718d504ff5
rbala25 <rishibala2007@gmail.com>
2026-05-21 12:59:20 -0400

    eth tx code

--- Hermes.srcs/sources_1/new/eth_tx.sv
+++ Hermes.srcs/sources_1/new/eth_tx.sv
@@ -43,7 +43,91 @@ typedef enum logic [2:0] {
     idle, dst, src, ether, payload
 } state_t;
 
-always_ff @(posedge tx_clk) begin
+state_t state;
+logic [2:0] cnt;
 
+always_ff @(posedge tx_clk) begin
+    if(rst) begin
+        state <= idle;
+        cnt <= 0;
+        txd <= 0;
+        tx_valid <= 0;
+        payload_ready <= 0;
+        done <= 0;
+    end else begin
+        tx_valid <= 0;
+        payload_ready <= 0;
+        done <= 0;
+        
+        unique case (state)
+            idle: begin
+                cnt <= 0;
+                if(start) begin
+                    state <= dst;
+                    txd <= dst_mac[47:40]; //drive first byte
+                    tx_valid <= 1;
+                    cnt <= 1;
+                end
+            end
+            
+            dst: begin
+                if (tx_ready) begin
+                    if(cnt < 5) begin
+                        txd <= dst_mac[47 - cnt*8 -: 8];
+                        tx_valid <= 1;
+                        cnt <= cnt + 1;
+                    end else begin
+                        txd <= dst_mac[7:0];
+                        cnt <= 0;
+                        tx_valid <= 1;
+                        state <= src;
+                    end
+                end
+            end
+            
+            src: begin
+                if (tx_ready) begin
+                    if(cnt < 5) begin
+                        txd <= SRC[47 - cnt*8 -: 8];
+                        tx_valid <= 1;
+                        cnt <= cnt + 1;
+                    end else begin
+                        txd <= SRC[7:0];
+                        cnt <= 0;
+                        tx_valid <= 1;
+                        state <= ether;
+                    end
+                end
+            end
+            
+            ether: begin
+                if(tx_ready) begin
+                    tx_valid <= 1;
+                    if(cnt) begin
+                        txd <= ether_type[7:0];
+                        cnt <= 0;
+                        state <= payload;
+                       //  payload_ready <= 1; mii takes 2 cycles for every byte
+                    end else begin
+                        txd <= ether_type[15:8];
+                        cnt <= cnt + 1;
+                    end
+                end
+            end
+            
+            payload: begin
+                if(tx_ready && payload_valid) begin
+                    tx_valid <= 1;
+                    txd <= payload_data;
+                    payload_ready <= 1;
+                end
+                
+                if(tx_ready && !payload_valid) begin
+                    done <= 1;
+                    state <= idle;
+                end
+            end
+        endcase  
+    end
 end    
 endmodule
