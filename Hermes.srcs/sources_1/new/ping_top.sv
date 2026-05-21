9c3c041da4798e104b38af295dd5bde84ce57543
rbala25 <rishibala2007@gmail.com>
2026-05-21 16:05:37 -0400

    ping top signals

---
+++ Hermes.srcs/sources_1/new/ping_top.sv
@@ -0,0 +1,110 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Rishi Bala
+// 
+// Create Date: 05/21/2026 03:26:03 PM
+// Design Name: 
+// Module Name: ping_top
+// Project Name: 
+// Target Devices: Arty A7 35t
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ping_top #(
+    parameter logic [31:0] IP = 32'hC0A80164
+    )(
+    input logic tx_clk,
+    input logic rx_clk,
+    input logic rst,
+    output logic [3:0] txd,
+    output logic tx_en,
+    input logic [3:0] rxd,
+    input logic rx_dv
+    );
+    
+logic [7:0] mii_rx_data; //mii_rx
+logic mii_rx_valid;
+logic mii_rx_frame_active; 
+
+logic [47:0] eth_dest_mac; //eth_parser
+logic [47:0] eth_src_mac;
+logic [15:0] eth_ether_type;
+logic eth_header_valid;
+logic [7:0] eth_payload_data;
+logic eth_payload_valid;
+logic eth_frame_done;
+logic eth_error;   
+
+logic [3:0] ip_version; //ip_parser
+logic [3:0] ip_ihl;
+logic [7:0] ip_dscp;
+logic [15:0] ip_total_len;
+logic [15:0] ip_id;
+logic [2:0] ip_flags;
+logic [12:0] ip_frag_offset;
+logic [7:0] ip_ttl;
+logic [7:0] ip_protocol;
+logic [15:0] ip_checksum;
+logic [31:0] ip_src;
+logic [31:0] ip_dest;
+logic ip_header_valid;
+logic ip_checksum_val;
+logic ip_is_fragment;
+logic [7:0] ip_payload_data;
+logic ip_payload_valid;
+logic ip_payload_done;
+logic ip_error;  
+
+logic [7:0] icmp_type; //icmp_parser
+logic [7:0] icmp_code;
+logic [15:0] icmp_checksum_rx;
+logic [15:0] icmp_identifier;
+logic [15:0] icmp_seq_num;
+logic icmp_header_valid;
+logic icmp_checksum_val;
+logic [7:0] icmp_payload_data;
+logic icmp_payload_valid;
+logic icmp_payload_done;
+logic icmp_error;  
+
+logic [7:0] ictx_data; //icmp tx to ip tx
+logic ictx_valid;
+logic ictx_ready;
+
+logic [7:0] iptx_data; //ip_tx to eth_tx
+logic iptx_valid;
+logic iptx_ready;
+ 
+logic [7:0] ethtx_data; //eth_tx to mii_tx
+logic ethtx_valid;
+logic ethtx_ready;
+
+logic icmp_tx_done; //tx start stop signals
+logic ip_tx_done;
+logic eth_tx_done;
+logic icmp_tx_start;
+logic ip_tx_start;
+logic eth_tx_start;
+
+logic [47:0] rep_dst_mac; //params for response
+logic [31:0] rep_src_ip;
+logic [31:0] rep_dst_ip;
+logic [15:0] rep_identifier;
+logic [15:0] rep_seq;
+logic [15:0] rep_total_len;
+logic [15:0] rep_ip_id;
+logic [15:0] rep_icmp_checksum_rx;
+
+logic [15:0] icmp_checksum_tx;
+    
+endmodule
