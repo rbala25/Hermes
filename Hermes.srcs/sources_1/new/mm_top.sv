d0ab1ee04c32101033de2ee9f7e473e23a983974
rbala25 <rishibala2007@gmail.com>
2026-05-28 01:43:11 -0400

    all rx outputs and gating

--- Hermes.srcs/sources_1/new/mm_top.sv
+++ Hermes.srcs/sources_1/new/mm_top.sv
@@ -57,5 +57,137 @@ module mm_top #(
     output logic [3:0] led
 );
 
+logic [7:0] mii_rx_data; //mii rx outputs
+logic mii_rx_valid;
+logic mii_rx_frame_active;
+
+logic [47:0] eth_dest_mac; //eth parser outputs
+logic [47:0] eth_src_mac;
+logic [15:0] eth_ether_type;
+logic eth_header_valid;
+logic [7:0] eth_payload_data;
+logic eth_payload_valid;
+logic eth_frame_done;
+logic eth_error;
+
+logic [3:0] ip_version; //ip parser outputs
+logic [3:0] ip_ihl;
+logic [7:0] ip_dscp;
+logic [15:0] ip_total_len;
+logic [15:0] ip_id;
+logic [2:0] ip_flags;
+logic [12:0] ip_frag_offset;
+logic [7:0] ip_ttl;
+logic [7:0] ip_protocol;
+logic [15:0] ip_checksum;
+logic [31:0] ip_src;
+logic [31:0] ip_dest;
+logic ip_header_valid;
+logic ip_checksum_val;
+logic ip_is_fragment;
+logic [7:0] ip_payload_data;
+logic ip_payload_valid;
+logic ip_payload_done;
+logic ip_error;
+ 
+logic [15:0] udp_src; //udp parser outputs
+logic [15:0] udp_dest;
+logic [15:0] udp_length;
+logic [15:0] udp_checksum;
+logic udp_header_valid;
+logic udp_checksum_val;
+logic [7:0] udp_payload;
+logic udp_payload_valid;
+logic udp_payload_done;
+logic udp_error; 
+
+logic [31:0] mdp_seq_num; //mdp parser outputs
+logic [63:0] mdp_sending_time;
+logic mdp_pkt_valid;
+logic [63:0] mdp_entry_price;
+logic [31:0] mdp_entry_size;
+logic [7:0] mdp_entry_price_level;
+logic [7:0] mdp_entry_update_action;
+logic [7:0] mdp_entry_type;
+logic mdp_is_snapshot;
+logic mdp_entry_valid;
+logic mdp_done;
+logic mdp_error;
+logic [63:0] mdp_trade_price;
+logic [31:0] mdp_trade_size;
+logic [1:0] mdp_trade_aggressor;
+logic mdp_trade_valid;
+
+logic [63:0] ob_best_bid_price; //order book outputs
+logic [31:0] ob_best_bid_size;
+logic [63:0] ob_best_ask_price;
+logic [31:0] ob_best_ask_size;
+logic ob_book_valid;
+logic ob_gap_detected;
+logic [3:0] ob_rd_level;
+logic ob_rd_side;
+logic [63:0] ob_rd_price;
+logic [31:0] ob_rd_size;
+
+logic tcprx_data_in_ready; //tcp rx outputs
+logic [15:0] tcprx_src_port;
+logic [15:0] tcprx_dst_port;
+logic [31:0] tcprx_seq_num;
+logic [31:0] tcprx_ack_num;
+logic [7:0] tcprx_flags;
+logic [15:0] tcprx_window_size;
+logic tcprx_header_valid;
+logic [7:0] tcprx_payload_data;
+logic tcprx_payload_valid;
+logic tcprx_payload_ready;
+logic tcprx_rx_syn;
+logic tcprx_rx_ack;
+logic tcprx_rx_fin;
+logic tcprx_rx_rst;
+logic tcprx_csum_error;
+
+logic [15:0] tcp_segment_length;
+assign tcp_segment_length = ip_total_len - {10'b0, ip_ihl, 2'b00}; //ip_total_len - ip_ihl*4
+ 
+logic tcp_ip_payload_valid;
+assign tcp_ip_payload_valid = ip_payload_valid && (ip_protocol == 8'h06);
+ 
+logic tcp_ip_payload_done_rx;
+assign tcp_ip_payload_done_rx = ip_payload_done && (ip_protocol == 8'h06);
+
+logic ilrx_payload_ready; //ilink rx outputs
+logic ilrx_neg_response;
+logic ilrx_estab_ack;
+logic ilrx_session_error;
+logic [383:0] ilrx_reject_reason;
+logic [31:0] ilrx_next_seq_no;
+logic [31:0] ilrx_rx_next_seq_no;
+logic ilrx_send_sequence;
+logic [63:0] ilrx_bid_order_id;
+logic [63:0] ilrx_ask_order_id;
+logic ilrx_exec_new;
+logic ilrx_exec_reject;
+logic ilrx_exec_elimination;
+logic ilrx_exec_trade;
+logic ilrx_exec_modify;
+logic ilrx_exec_cancel;
+logic ilrx_unsolicited_cancel;
+logic ilrx_ocr_reject;
+logic ilrx_business_reject;
+logic ilrx_gap_detected;
+logic [31:0] ilrx_gap_from_seq;
+logic [31:0] ilrx_gap_count;
+logic signed [63:0] ilrx_fill_price;
+logic [31:0] ilrx_fill_qty;
+logic [31:0] ilrx_fill_leaves_qty;
+logic [31:0] ilrx_fill_cum_qty;
+logic [7:0] ilrx_fill_side;
+logic [159:0] ilrx_fill_clord_id;
+logic [319:0] ilrx_exec_id;
+logic [15:0] ilrx_ord_rej_reason;
+logic [15:0] ilrx_cxl_rej_reason;
+logic [63:0] ilrx_order_id_out;
+logic [15:0] ilrx_biz_rej_reason;
+logic [2047:0] ilrx_biz_text;
 
 endmodule
